0d02283a4503310082f2b050227ee73ea813c375
Rosa Maria BJ <62024358+Rosaenergyflow@users.noreply.github.com>
2022-12-26 16:14:46 +0100

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'astroflor',
+        'database' => 'db_name',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
